f1204c1c5ad7693aeec6ee87ff4a57ac9ce379e4
lazyreader <kenvylf@gmail.com>
2023-07-21 12:43:04 +0800

    final hinduan 100%

--- Unity_Proj_Files/Hinduan/Assets/Numbers Board/Scripts/LetterInstructionSelector.cs
+++ Unity_Proj_Files/Hinduan/Assets/Numbers Board/Scripts/LetterInstructionSelector.cs
@@ -4,24 +4,14 @@ using UnityEngine;
 
 public class LetterInstructionSelector : MonoBehaviour
 {
-    public GameObject[] traceLetters;//the letters list from A-Z
+    public Sprite[] traceLetters;//the letters list from A-Z
     public GameObject LettersObj;
     public int LetterIndex = 0;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        foreach (GameObject tl in traceLetters)
-        {
-            if (tl != null)
-                tl.SetActive(false);
-        }
-    }
 
     // Update is called once per frame
     private void Update()
     {
         LetterIndex = LettersObj.GetComponent<WritingHandler>().CLITemp;
-        traceLetters[LetterIndex].SetActive(true);
+        GetComponent<SpriteRenderer>().sprite = traceLetters[LetterIndex];
     }
 }
